0e4e38a1f8a6c6b1e0e7932eb2cb4f67e762bf2b
ASO-CODE <60587094+ASO-CODE@users.noreply.github.com>
2022-11-05 04:29:40 +0100

    feat: Textarea validation

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InputForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [focused, setFocused] = useState(false);
+
   const name = "Solomon";
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    clearValue();
+  };
+
+  const onChangeFirstName = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const onChangeLastName = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangeMessage = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const clearValue = () => {
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setMessage("");
+  };
 
   return (
     <section id="Form">
@@ -16,7 +54,10 @@ const InputForm = () => {
             <input
               type="text"
               id="first_name"
+              value={firstName}
               placeholder="Enter your first name"
+              required
+              onChange={onChangeFirstName}
             />
           </div>
           {/*  */}
@@ -25,29 +66,45 @@ const InputForm = () => {
             <input
               type="text"
               id="last_name"
+              value={lastName}
               placeholder="Enter your last name"
+              required
+              onChange={onChangeLastName}
             />
           </div>
         </div>
         <div className="email-input">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" placeholder="yourname@email.com" />
+          <input
+            type="email"
+            id="email"
+            value={email}
+            placeholder="yourname@email.com"
+            required
+            onChange={onChangeEmail}
+            onBlur={handleFocus}
+            focused={focused.toString()}
+          />
         </div>
         <div className="message-input">
           <label htmlFor="message">Message</label>
           <textarea
             name=""
             id="message"
+            value={message}
             cols="30"
             rows="10"
             placeholder="Send me a message and I'll reply you as soon as possible..."
+            required
+            onChange={onChangeMessage}
           ></textarea>
+          <span>Please enter a message</span>
         </div>
         <div className="check-field">
-          <input type="checkbox" id="checkbox" />
+          <input type="checkbox" id="checkbox" required />
           <p>You agree to providing your data to {name} who may contact you.</p>
         </div>
-        <button type="submit" id="btn__submit">
+        <button type="submit" id="btn__submit" onClick={handleSubmit}>
           Send message
         </button>
       </form>
